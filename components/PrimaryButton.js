2d588118896b0f6c2dbdb38d4fe8d52e8a421b45
21SINGH <s.far.focus@gmail.com>
2024-08-26 14:55:01 +0530

    adding linear gradient and changing the sytle

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,9 +1,48 @@
-import { View , Text } from "react-native"
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
-export default function PrimaryButton({children}) {
-    return (
-        <View >
-            <Text >{children}</Text>
+export default function PrimaryButton({ children }) {
+  return (
+    <View style={styles.outterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.innerContainer, styles.pressed]
+            : styles.innerContainer
+        }
+      >
+        <View style={styles.continer}>
+          <Text style={styles.text}>{children}</Text>
         </View>
-    )
+      </Pressable>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  outterContainer: {
+    borderRadius: 10,
+    margin: 4,
+    overflow: "hidden",
+  },
+  innerContainer: {
+    backgroundColor: "gray",
+    opacity: 1,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+    shadowColor: "white",
+    shadowOffset: { width: 1, height: 0 },
+    shadowRadius: 10,
+    shadowOpacity: 0.3,
+    color: "white",
+  },
+  text: {
+    color: "white",
+    opacity: 1,
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.5,
+    backgroundColor: "plum",
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,27 +1,54 @@
-import { TextInput, View , StyleSheet} from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 export default function StartGameScreen() {
   return (
-    <View style={styles.inputContainer}>
-      <TextInput />
+    <View style={styles.inputContainer} >
+      <TextInput style={styles.input} maxLength={2} keyboardType="number-pad" autoCapitalize="none" />
+      <View style={styles.buttonContiner}>
+        <View style={styles.buttonElement}>
       <PrimaryButton> RESET</PrimaryButton>
-      <PrimaryButton> Confirm </PrimaryButton>
+      </View>
+      <View style={styles.buttonElement}>
+      <PrimaryButton> START </PrimaryButton>
+      </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    inputContainer :{
-        padding: 16,
-        marginHorizontal : 24,
-        marginTop: 100,
-        backgroundColor : '#72063c',
-        borderRadius: 8,
-        elevation : 4,
-        shadowColor : "black",
-        shadowOffset : {width : 0, height : 2}, 
-        shadowRadius : 10,
-        shadowOpacity : 0.4,
-    }
-})
+  inputContainer: {
+    padding: 16,
+    marginHorizontal: 24,
+    marginTop: 100,
+    backgroundColor: "black",
+    borderRadius: 8,
+    elevation: 10,
+    shadowColor: "purple",
+    shadowOffset: { width: 1, height: 4},
+    shadowRadius: 5,
+    shadowOpacity: 1,
+    alignItems : "center",
+  },
+  input: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "white",
+    borderBottomWidth: 1,
+    color: "white",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign : "center",
+  },
+  buttonContiner : {
+    marginTop : 5,
+    flexDirection : "row",
+    // justifyContent : "space-around",
+    // width : "60%"
+  },
+  buttonElement:{
+    flex: 1,
+  }
+});
